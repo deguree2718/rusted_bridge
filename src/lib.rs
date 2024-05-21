ec81cc8bebfe99d9aec76575fca2733e246dfd38
Paulo Grechi <paulo.grechi@pro4ce.com>
2024-05-21 11:51:03 -0300

    there was an attempt on using static variables to avoid having to call the get_devices() function too much, I failed

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
 use devices::*;
 
+// static mut DEVICES: Vec<DeviceInfo> = Vec::new();
+//
+// unsafe fn get_devices() {
+//     let mut device_list = Vec::new();
+//     device_list.append(&mut Devices::usb().unwrap_or_default());
+//     device_list.append(&mut Devices::pci().unwrap_or_default());
+//     DEVICES.append(&mut device_list);
+// }
+
 pub fn get_devices() -> Vec<DeviceInfo> {
-    Devices::get().unwrap_or_default()
+    let mut device_list: Vec<DeviceInfo> = Vec::new();
+    device_list.append(&mut Devices::usb().unwrap_or_default());
+    device_list.append(&mut Devices::pci().unwrap_or_default());
+    device_list
 }
 
 #[cfg(test)]
@@ -10,6 +22,7 @@ mod tests {
 
     #[test]
     fn find_devices() {
-        assert_ne!(Vec::<DeviceInfo>::new(), get_devices());
+        let device_lsit = get_devices();
+        assert_ne!(Vec::<DeviceInfo>::new(), device_lsit);
     }
 }
