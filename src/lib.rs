0da7dcc8466564bd8b1f72f6d5d837d81d2dc13d
-e <paulo.grechi@pro4ce.com>
2024-08-09 11:40:43 -0300

    tentativas de correção, ainda sem sucesso

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 use std::path::Path;
 //use wasm_bindgen::prelude::wasm_bindgen;
-use pdfium_render::prelude::*;
 use printers;
 use printers::printer::Printer;
 
 pub fn get_printers() -> Vec<Printer> {
   let mut printer_list = Vec::new();
-  // for printer in printers::get_printers() {
-  //   if printer.name.contains("Microsoft") && printer.name != "Fax" && printer.name.to_lowercase() == printer.driver_name.to_lowercase() {
-  //     printer_list.push(printer);
-  //   }
-  // }
   for printer in printers::get_printers() {
-    if printer.name != "Fax" && !printer.name.contains("Microsoft") {
+    if printer.name.contains("Microsoft") && printer.name != "Fax" && printer.name.to_lowercase() == printer.driver_name.to_lowercase() {
       printer_list.push(printer);
     }
   }
+  // for printer in printers::get_printers() {
+  //   if printer.name != "Fax" && !printer.name.contains("Microsoft") {
+  //     printer_list.push(printer);
+  //   }
+  // }
   printer_list
 }
 
@@ -23,16 +22,31 @@ pub fn get_printers() -> Vec<Printer> {
 mod tests {
   use std::fs;
   use super::*;
+  use imcon::{Format, Image};
+  use recibo::{Alignment, FileDriver, GraphicSize};
+  use recibo::Printer;
 
   #[test]
   fn find_devices() {
-      let device_list = get_printers();
-      let mut result: bool = false;
-      for printer in device_list {
-        // let file = fs::read("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf");
-        // result = printer.print(file.unwrap().as_slice(), None).expect("Could not print file");
-        result = printer.print_file("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf", None).expect("Could not print file");
-      }
-      assert!(result);
+    let device_list = get_printers();
+    let mut result: bool = true;
+    let file = fs::read("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf");
+    let mut img = Image::read(file.unwrap().as_slice(), Format::Pdf);
+    img.unwrap().save("C:\\Users\\paulo\\Desktop\\pdf.png");
+    let driver = FileDriver::open("C:\\Users\\paulo\\OneDrive\\Área de Trabalho\\arquivo-teste.txt");
+    let mut printer = Printer::open(driver).unwrap();
+    printer.init().unwrap()
+      .align(Alignment::Center).unwrap()
+      .graphic(move |builder| {
+        builder.path("C:\\Users\\paulo\\Desktop\\pdf.png")
+          .size(GraphicSize::Normal)
+      }).unwrap();
+    // "C:\\Users\\paulo\\OneDrive\\Área de Trabalho\\arquivo-teste.txt"
+    // for printer in device_list {
+    //   let file = fs::read("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf");
+    //   result = printer.print(file.unwrap().as_slice(), None).expect("Could not print file");
+    //   //result = printer.print_file("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf", None).expect("Could not print file");
+    // }
+    // assert!(result);
   }
 }
