79b6bfc340f0b95aabf149aef002d9a820eb2b5c
-e <paulo.grechi@pro4ce.com>
2024-08-23 09:04:11 -0300

    alterado o teste para tentar usar os comandos do powershell (ou bash) para a impressão dos documentos

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::path::Path;
 //use wasm_bindgen::prelude::wasm_bindgen;
 use printers;
 use printers::printer::Printer;
+use std::process::Command;
 
 pub fn get_printers() -> Vec<Printer> {
   let mut printer_list = Vec::new();
@@ -22,25 +23,35 @@ pub fn get_printers() -> Vec<Printer> {
 mod tests {
   use std::fs;
   use super::*;
-  use imcon::{Format, Image};
-  use recibo::{Alignment, FileDriver, GraphicSize};
-  use recibo::Printer;
+  // use imcon::{Format, Image};
+  // use recibo::{Alignment, FileDriver, GraphicSize};
+  // use recibo::Printer;
 
   #[test]
   fn find_devices() {
     let device_list = get_printers();
-    let mut result: bool = true;
-    let file = fs::read("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf");
-    let mut img = Image::read(file.unwrap().as_slice(), Format::Pdf);
-    img.unwrap().save("C:\\Users\\paulo\\Desktop\\pdf.png");
-    let driver = FileDriver::open("C:\\Users\\paulo\\OneDrive\\Área de Trabalho\\arquivo-teste.txt");
-    let mut printer = Printer::open(driver).unwrap();
-    printer.init().unwrap()
-      .align(Alignment::Center).unwrap()
-      .graphic(move |builder| {
-        builder.path("C:\\Users\\paulo\\Desktop\\pdf.png")
-          .size(GraphicSize::Normal)
-      }).unwrap();
+    // let mut result: bool = true;
+    if cfg!(target_os = "windows"){
+      let cmd = Command::new("powershell")
+        .args(["Start-Process","-FilePath \"1b6d3899-f8b6-4a3b-b0b3-03ffb8a5e842.pdf\"", "-WorkingDirectory \"C:\\Users\\paulo\\Downloads\"", "-Verb Print"])
+        .output()
+        .expect("failed");
+      panic!("{:?}", cmd);
+    //   Start-Process -FilePath "1b6d3899-f8b6-4a3b-b0b3-03ffb8a5e842.pdf" -WorkingDirectory "C:\Users\paulo\Downloads" -Verb Print
+    } else {
+
+    }
+    // let file = fs::read("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf");
+    // let mut img = Image::read(file.unwrap().as_slice(), Format::Pdf);
+    // img.unwrap().save("C:\\Users\\paulo\\Desktop\\pdf.png");
+    // let driver = FileDriver::open("C:\\Users\\paulo\\OneDrive\\Área de Trabalho\\arquivo-teste.txt");
+    // let mut printer = Printer::open(driver).unwrap();
+    // printer.init().unwrap()
+    //   .align(Alignment::Center).unwrap()
+    //   .graphic(move |builder| {
+    //     builder.path("C:\\Users\\paulo\\Desktop\\pdf.png")
+    //       .size(GraphicSize::Normal)
+    //   }).unwrap();
     // "C:\\Users\\paulo\\OneDrive\\Área de Trabalho\\arquivo-teste.txt"
     // for printer in device_list {
     //   let file = fs::read("C:\\Users\\paulo\\Downloads\\0dd30670-4e01-499b-8ea3-6c86882e8400.pdf");
