f74978cd0fefa2f55de5671ebaae2cf43933fae7
-e <paulo.grechi@pro4ce.com>
2024-08-29 11:02:27 -0300

    alteradas dependencias, ainda usando a crate [printers by talesluna] mas em conjunto com a crate [ipp.rs] para efetuar a impressão, ainda são necessários testes

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,47 @@
-use std::{
-	path::Path
-// 	// process::Command
-};
-//use wasm_bindgen::prelude::wasm_bindgen;
-// use pdfium_render::prelude::*;
-// use printers;
-// use printers::printer::Printer;
-use winprint::{
-  printer::{
-    FilePrinter,
-    PrinterDevice,
-    XpsPrinter
-  },
-  ticket::{
-    PrintCapabilities,
-    PrintTicket,
-    PrintTicketBuilder
-  }
-};
-use winprint::printer::PdfiumPrinter;
+use std::{fs::{File}, env};
+use wasm_bindgen::prelude::wasm_bindgen;
+use printers;
+use ipp::prelude::*;
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-pub fn get_printers() -> Vec<PrinterDevice> {
+#[wasm_bindgen]
+pub fn get_printers() -> Vec<String> {
   let mut printer_list = Vec::new();
-  for printer in PrinterDevice::all().expect("err") {
-    if printer.name() == "Microsoft Print to PDF" {
-      printer_list.push(printer);
+  for printer in printers::get_printers() {
+    println!("{:?}", printer);
+    if printer.name == "Microsoft Print to PDF" {
+      printer_list.push(String::from(printer.name));
     }
   }
-  // for printer in printers::get_printers() {
-  //   if printer.name != "Fax" && !printer.name.contains("Microsoft") {
-  //     printer_list.push(printer);
-  //   }
-  // }
   printer_list
 }
 
-pub fn print(p_printer: PrinterDevice, file: &Path, opt: PrintTicket) {
-  //let file_str = String::from(file.to_str().expect("Could not parse path"));
-  let xps = PdfiumPrinter::new(p_printer);
-  xps.print(file, opt).unwrap();
+#[wasm_bindgen]
+#[cfg(target_os = "linux")]
+pub fn print_file(printer_name: String) {
+  let printer = printers::get_printer_by_name(&printer_name).unwrap();
+  let payload = IppPayload::new(File::open("C:\\Users\\paulo\\Downloads\\teste1.pdf").unwrap());
+  let uri: Uri = printer.location.parse().unwrap();
+  let mut builder = IppOperationBuilder::print_job(uri.clone(), payload)
+    .user_name(env::var("USER").unwrap_or_else(|_| "noname".to_owned()))
+    .job_title("None");
+
+  let operation = builder.build();
+  let client = IppClient::new(uri);
+  let response = client.send(operation)?;
+
+  println!("IPP Status code: {}", response.header().status_code());
+
+  let attrs = response
+    .attributes()
+    .groups_of(DelimiterTag::JobAttributes)
+    .flat_map(|g| g.attributes().values());
+}
+
+#[wasm_bindgen]
+#[cfg(target_os = "windows")]
+pub fn print_file(printer_name: String) {
+
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,23 +1,13 @@
-use rusted_bridge::*;
+use rusted_bridge::{print_file, get_printers};
 use std::fs;
+use std::fs::read;
 use std::path::Path;
-use winprint::ticket::{PredefinedMediaName, PrintTicket, PrintTicketBuilder, PrintCapabilities};
-
 #[test]
 fn find_devices() {
   let device_list = get_printers();
   println!("{:?}", device_list);
-  // let mut result: bool = false;
-  // for printer in device_list {
-  //   let opt = PrintCapabilities::fetch(&printer).unwrap();
-  //   let a4media = opt
-  //     .page_media_size()
-  //     .find(|x| x.as_predefined_name() == Some(PredefinedMediaName::ISOA4))
-  //     .unwrap();
-  //   let mut bd = PrintTicketBuilder::new(&printer).unwrap();
-  //   bd.merge(a4media).unwrap();
-  //   let tic = bd.build().unwrap();
-  //   print(printer, Path::new("C:\\Users\\paulo\\Downloads\\b045218a-f1ac-4b5b-b17b-8eeae96c954a.pdf"), tic);
-  //  }
-  //  assert!(result);
+  for printer in device_list {
+    // let file = read(Path::new("C:\\Users\\paulo\\Downloads\\teste1.pdf")).unwrap();
+    let result = print_file(printer);
+   }
 }
