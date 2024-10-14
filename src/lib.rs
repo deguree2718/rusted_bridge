abf0a0f3f0d81fc952f3f45ef535b0dc6acaeaca
deguree2718 <deguree2.718@gmail.com>
2024-10-14 07:56:36 -0300

    testei e enviou pra impressora

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{fs::{File}, env};
 use wasm_bindgen::prelude::wasm_bindgen;
-use printers;
+//use printers;
 use ipp::prelude::*;
 
 #[cfg(feature = "wee_alloc")]
@@ -11,20 +11,21 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn get_printers() -> Vec<String> {
   let mut printer_list = Vec::new();
   for printer in printers::get_printers() {
-    // if printer.name == "Microsoft Print to PDF" {
+  	if !String::from(printer.uri).starts_with("file://") {
       printer_list.push(printer.name);
-    // }
+  	}
   }
   printer_list
 }
 
 #[wasm_bindgen]
 #[cfg(target_os = "linux")]
-pub fn print_file(printer_name: String) {
+pub fn print_file(printer_name: String) { // , file_path: String) {
   let printer = printers::get_printer_by_name(&printer_name).unwrap();
   println!("{:?}", printer);
   let payload = IppPayload::new(File::open("/home/paulo-grechi/Downloads/GRIMOIRE_DIGITAL.pdf").unwrap());
   let uri: Uri = printer.uri.parse().unwrap();
+  // let uri: Uri = "http://localhost:631/printers/Test_Printer_102611".parse().unwrap();
   println!("{:?}", uri);
   let builder = IppOperationBuilder::print_job(uri.clone(), payload)
     .user_name(env::var("USER").unwrap_or_else(|_| "noname".to_owned()))
@@ -36,10 +37,19 @@ pub fn print_file(printer_name: String) {
 
   println!("IPP Status code: {}", response.header().status_code());
 
+  println!("{:?}", response.attributes());
+
   // let attrs = response
-  //   .attributes()
-  //   .groups_of(DelimiterTag::JobAttributes)
-  //   .flat_map(|g| g.attributes().values());
+  //    .attributes()
+  //    .groups_of(DelimiterTag::JobAttributes)
+  //    .flat_map(|g| g.attributes().values());
+  
+}
+
+#[wasm_bindgen]
+#[cfg(target_os = "linux")]
+pub unsafe fn print(printer_name: String) {
+	
 }
 
 #[wasm_bindgen]
--- tests/test.rs
+++ tests/test.rs
@@ -3,13 +3,12 @@ use std::fs;
 use std::fs::read;
 use std::path::Path;
 #[test]
-fn find_devices() {
+fn find_devices() { // file_path: String
   let device_list = get_printers();
   println!("{:?}", device_list);
   for printer in device_list {
   	println!("{}", printer);
-    // let file = read(Path::new("C:\\Users\\paulo\\Downloads\\teste1.pdf")).unwrap();
-     let result = crate::print_file(printer);
+    let result = crate::print_file(printer);
 	break;
    }
 }
