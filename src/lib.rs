7cdd60d85a45918ac7bd87e415cc46133244d1ed
deguree2718 <deguree2.718@gmail.com>
2026-04-16 10:43:22 -0300

    Simplificado o código com o cfg_select!

--- src/lib.rs
+++ src/lib.rs
@@ -15,27 +15,26 @@ pub fn get_printers() -> Vec<String> {
 }
 
 //#[wasm_bindgen]
-#[cfg(target_os = "linux")]
 pub fn print_file(printer_name: String, file_path: String) -> bool {
-  let payload = IppPayload::new(File::open(file_path).unwrap());
-  let uri: Uri = ("http://localhost:631/printers/".to_owned() + &printer_name).parse().unwrap();
-  let builder = IppOperationBuilder::print_job(uri.clone(), payload)
-    .user_name(env::var("USER").unwrap_or_else(|_| "noname".to_owned()))
-    .job_title("None")
-    .attribute(IppAttribute::new("document-format", IppValue::MimeMediaType(String::from("application/octet-stream"))));
+	cfg_select! {
+		unix => {
+			let payload = IppPayload::new(File::open(file_path).unwrap());
+			let uri: Uri = ("http://localhost:631/printers/".to_owned() + &printer_name).parse().unwrap();
+			let builder = IppOperationBuilder::print_job(uri.clone(), payload)
+				.user_name(env::var("USER").unwrap_or_else(|_| "noname".to_owned()))
+				.job_title("None")
+				.attribute(IppAttribute::new("document-format", IppValue::MimeMediaType(String::from("application/octet-stream"))));
 
-  let operation = builder.build();
-  let client = IppClient::new(uri);
-  let response = client.send(operation).unwrap();
+			let operation = builder.build();
+			let client = IppClient::new(uri);
+			let response = client.send(operation).unwrap();
 
-  println!("IPP Status code: {}", response.header().status_code());
+			println!("IPP Status code: {}", response.header().status_code());
 
-  return response.header().status_code().is_success();
-  
-}
-
-//#[wasm_bindgen]
-#[cfg(target_os = "windows")]
-pub fn print_file(printer_name: String) {
-
+			return response.header().status_code().is_success();
+		},
+		_ => {
+			false
+		}
+	}  
 }
